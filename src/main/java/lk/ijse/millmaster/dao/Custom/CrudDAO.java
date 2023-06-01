4136aead2e913cae999c6f07c86f3cb266108ecb
Amil Srinath <amilsrinath5@gmail.com>
2023-06-01 12:10:59 +0530

    Create CrudDAO Interface

---
+++ src/main/java/lk/ijse/millmaster/dao/Custom/CrudDAO.java
@@ -0,0 +1,9 @@
+package lk.ijse.millmaster.dao.Custom;
+
+import java.sql.SQLException;
+
+public interface CrudDAO<T> {
+    public boolean add(T entity) throws SQLException, ClassNotFoundException;
+    public boolean update(T entity) throws SQLException, ClassNotFoundException;
+    public boolean delete(String id) throws SQLException, ClassNotFoundException;
+}
