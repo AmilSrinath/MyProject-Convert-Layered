88aee51a0a5ea260a67090457d652a4c2205285e
Amil Srinath <amilsrinath5@gmail.com>
2023-06-03 13:58:33 +0530

    Create Class

--- src/main/java/lk/ijse/millmaster/dao/Custom/Impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/millmaster/dao/Custom/Impl/UserDAOImpl.java
@@ -1,4 +1,24 @@
 package lk.ijse.millmaster.dao.Custom.Impl;
 
-public class UserDAOImpl {
+import lk.ijse.millmaster.dao.CrudDAO;
+import lk.ijse.millmaster.dao.Custom.UserDAO;
+import lk.ijse.millmaster.dto.User;
+
+import java.sql.SQLException;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public boolean add(User entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(User entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
 }
