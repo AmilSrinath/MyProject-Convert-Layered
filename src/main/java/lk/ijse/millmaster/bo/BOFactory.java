4793033fe61108483c5402ecd59303f94ec5cb89
Amil Srinath <amilsrinath5@gmail.com>
2023-06-03 14:15:43 +0530

    Add BOFactory Code

--- src/main/java/lk/ijse/millmaster/bo/BOFactory.java
+++ src/main/java/lk/ijse/millmaster/bo/BOFactory.java
@@ -1,4 +1,26 @@
 package lk.ijse.millmaster.bo;
 
+import lk.ijse.millmaster.bo.Custom.Impl.UserBOImpl;
+
 public class BOFactory {
+    private static BOFactory boFactory;
+    private BOFactory(){
+    }
+    public static BOFactory getBoFactory(){
+        return (boFactory==null)? boFactory=new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes{
+        USER
+    }
+
+    //Object creation logic for BO objects
+    public SuperBO getBO(BOTypes types){
+        switch (types){
+            case USER:
+                return new UserBOImpl();
+            default:
+                return null;
+        }
+    }
 }
