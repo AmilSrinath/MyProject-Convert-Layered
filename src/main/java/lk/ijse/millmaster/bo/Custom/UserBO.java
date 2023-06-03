0dc9c74caace2d87ccdd9f818111923adda77a7e
Amil Srinath <amilsrinath5@gmail.com>
2023-06-03 14:44:42 +0530

    Update

--- src/main/java/lk/ijse/millmaster/bo/Custom/UserBO.java
+++ src/main/java/lk/ijse/millmaster/bo/Custom/UserBO.java
@@ -6,9 +6,9 @@ import lk.ijse.millmaster.dto.UserDTO;
 import java.sql.SQLException;
 
 public interface UserBO extends SuperBO {
-    public boolean addUser(UserDTO dto) throws SQLException, ClassNotFoundException ;
+    public boolean addUser(UserDTO dto) throws SQLException, ClassNotFoundException;
 
-    public boolean updateUser(UserDTO dto) throws SQLException, ClassNotFoundException ;
+    public boolean updateUser(UserDTO dto) throws SQLException, ClassNotFoundException;
 
     public boolean existUser(String id) throws SQLException, ClassNotFoundException;
 
